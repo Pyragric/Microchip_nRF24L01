f40bfe06f8eaaadb13a46b4ee09ec79bb0978e84
Chommanivong Nello <nello.chom@protonmail.com>
2021-02-01 22:53:37 +0100

    Remake structures, begin to write NRF functions

--- NRF24L01_Lib.X/01 - SRC/03_NRF24L01/NRF24L01.c
+++ NRF24L01_Lib.X/01 - SRC/03_NRF24L01/NRF24L01.c
@@ -1,71 +1,60 @@
 #include "NRF24L01.h"
 
-static t_NRF_Config NRF_REG_CONFIG;
-static t_NRF_STATUS NRF_REG_STATUS;
-static t_NRF_RX_ADDR_5_BYTES NRF_ADDR_PIPE_0;
-
-uint8_t (*p_NRF_SPI_Exchange)(uint8_t);
+static uint8_t (*p_NRF_SPI_Exchange)(uint8_t);
 
 void NRF24L01_Init(void)
 {
     Set_SPI_Handler(SoftSPI_Exchange);
     NRF_PIN_CE = 0;
     NRF_PIN_CSN = 1;
-    NRF_REG_STATUS.byte = 0;
-    NRF_REG_CONFIG.byte = 0;
-    
-    NRF_ADDR_PIPE_0.Byte0 = 1;
-    NRF_ADDR_PIPE_0.Byte1 = 2;
-    NRF_ADDR_PIPE_0.Byte2 = 3;
-    NRF_ADDR_PIPE_0.Byte3 = 4;
-    NRF_ADDR_PIPE_0.Byte4 = 5;
 }
 
-void NRF_SET_PRX(void)
+void NRF_Set_PRX(t_NRF_Setup *MyNRF)
 {
-    t_NRF_Config R_CONFIG;
-    t_NRF_RX_ADDR_5_BYTES Pipe1;
-    uint8_t R_Status;
-    uint8_t R_Register;
-    R_CONFIG.byte = 0;
-    
-    NRF_REG_CONFIG.PWR_UP = 1u;
-    NRF_REG_CONFIG.EN_CRC = 1u;
-    NRF_REG_CONFIG.PRIM_RX = 1u;
-    
-    R_Status = NRF_Write_Register(REG_NRF_CONFIG, &NRF_REG_CONFIG.byte, 1);
-    R_Status = NRF_Read_Register(REG_NRF_CONFIG, &R_CONFIG.byte, 1);
-    PrintUART("\r\nCONFIG: ");
-    UART_PNbase(R_CONFIG.byte, UART_BIN, " STA: ");
-    UART_PNbase(R_Status, UART_BIN, "\r\n");
-    
-    R_Status = NRF_Read_Register(REG_NRF_RX_ADDR_P0, NRF_ADDR_PIPE_0.ByteArray, 5);
-    PrintUART("\r\nADDR PIPE 0: : ");
-    UART_PNbase(NRF_ADDR_PIPE_0.Byte4, UART_HEX, " ");
-    UART_PNbase(NRF_ADDR_PIPE_0.Byte3, UART_HEX, " ");
-    UART_PNbase(NRF_ADDR_PIPE_0.Byte2, UART_HEX, " ");
-    UART_PNbase(NRF_ADDR_PIPE_0.Byte1, UART_HEX, " ");
-    UART_PNbase(NRF_ADDR_PIPE_0.Byte0, UART_HEX, " STA: ");
-    UART_PNbase(R_Status, UART_BIN, "\r\n");
-    
-    R_Status = NRF_Read_Register(REG_NRF_RX_ADDR_P1, Pipe1.ByteArray, 5);
-    PrintUART("ADDR PIPE 1: : ");
-    UART_PNbase(Pipe1.Byte4, UART_HEX, " ");
-    UART_PNbase(Pipe1.Byte3, UART_HEX, " ");
-    UART_PNbase(Pipe1.Byte2, UART_HEX, " ");
-    UART_PNbase(Pipe1.Byte1, UART_HEX, " ");
-    UART_PNbase(Pipe1.Byte0, UART_HEX, " STA: ");
-    UART_PNbase(R_Status, UART_BIN, "\r\n");
-    
-    R_Status = NRF_Read_Register(REG_NRF_RF_CH, &R_Register, 1);
-    PrintUART("RF CH: ");
-    UART_PNbase(R_Register, UART_BIN, "  STA: ");
-    UART_PNbase(R_Status, UART_BIN, "\r\n");
-    
-    R_Status = NRF_Read_Register(REG_NRF_RF_SETUP, &R_Register, 1);
-    PrintUART("RF_SETUP: ");
-    UART_PNbase(R_Register, UART_BIN, "  STA: ");
-    UART_PNbase(R_Status, UART_BIN, "\r\n");
+    NRF_Read_Register(REG_NRF_CONFIG, &MyNRF->CONFIG, 1u);
+    MyNRF->CONFIG |= NRF_PRX;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_CONFIG, &MyNRF->CONFIG, 1u);
+}
+
+void NRF_Set_PTX(t_NRF_Setup *MyNRF)
+{
+    NRF_Read_Register(REG_NRF_CONFIG, &MyNRF->CONFIG, 1u);
+    MyNRF->CONFIG &= ~NRF_PRX;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_CONFIG, &MyNRF->CONFIG, 1u);
+}
+
+void NRF_SetRFChannel(t_NRF_Setup *MyNRF, uint8_t RF_Channel)
+{
+    MyNRF->RF_CHANNEL = (RF_Channel > 125u) ? 125u : RF_Channel;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_RF_CH, &MyNRF->RF_CHANNEL, 1u);
+}
+
+void NRF_SetRFPower(t_NRF_Setup *MyNRF, uint8_t RF_Pow)
+{
+    NRF_Read_Register(REG_NRF_RF_SETUP, &MyNRF->RF_SETUP, 1u);
+    RF_Pow = (RF_Pow > 3u) ? 3u : RF_Pow;
+    RF_Pow = (RF_Pow << 1u) | 0x06u;
+    MyNRF->RF_SETUP &= ~0x06u;
+    MyNRF->RF_SETUP |= RF_Pow;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_RF_SETUP, &MyNRF->RF_SETUP, 1u);
+}
+
+void NRF_SetRFDataRate(t_NRF_Setup *MyNRF, uint8_t Datarate)
+{
+    NRF_Read_Register(REG_NRF_RF_SETUP, &MyNRF->RF_SETUP, 1u);
+    Datarate = (Datarate > 1u) ? 1u : 0u;
+    Datarate = (Datarate << 3u) & 0x08u;
+    MyNRF->RF_SETUP &= ~0x08u;
+    MyNRF->RF_SETUP |= Datarate;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_RF_SETUP, &MyNRF->RF_SETUP, 1u);
+}
+
+void NRF_SetAddrWidth(t_NRF_Setup *MyNRF, uint8_t Width)
+{
+    Width += (Width == 0u) ? 1u : 0u;
+    Width = (Width > 3u) ? 3u : 0u;
+    MyNRF->ADDR_WIDTH = Width;
+    MyNRF->STATUS = NRF_Write_Register(REG_NRF_SETUP_AW, &MyNRF->ADDR_WIDTH, 1u);
 }
 
 uint8_t NRF_Write_Register(uint8_t Register, uint8_t *Bytes, uint8_t Length)
--- NRF24L01_Lib.X/01 - SRC/03_NRF24L01/NRF24L01.h
+++ NRF24L01_Lib.X/01 - SRC/03_NRF24L01/NRF24L01.h
@@ -22,259 +22,116 @@
 /*------------------------------------------------*/
 /*              NRF24L01 SPI commands             */
 /*------------------------------------------------*/
-#define CMD_NRF_R_REGISTER          0x00
-#define CMD_NRF_W_REGISTER          0x20
-#define CMD_NRF_R_RX_PAYLOA         0x61
-#define CMD_NRF_W_TX_REGISTR        0xA0
-#define CMD_NRF_FLUSH_TX            0xE1
-#define CMD_NRF_FLUSH_RX            0xE2
-#define CMD_NRF_REUSE_TX_PL         0xE3
-#define CMD_NRF_ACTIVATE            0x50
-#define CMD_NRF_R_RX_PL_WID         0x60
-#define CMD_NRF_W_ACK_PAYLOAD       0xA8
-#define CMD_NRF_W_TX_PAYLOAD_NO_ACK 0xB0
-#define CMD_NRF_NOP                 0x00
+#define CMD_NRF_R_REGISTER          0x00u
+#define CMD_NRF_W_REGISTER          0x20u
+#define CMD_NRF_R_RX_PAYLOA         0x61u
+#define CMD_NRF_W_TX_REGISTR        0xA0u
+#define CMD_NRF_FLUSH_TX            0xE1u
+#define CMD_NRF_FLUSH_RX            0xE2u
+#define CMD_NRF_REUSE_TX_PL         0xE3u
+#define CMD_NRF_ACTIVATE            0x50u
+#define CMD_NRF_R_RX_PL_WID         0x60u
+#define CMD_NRF_W_ACK_PAYLOAD       0xA8u
+#define CMD_NRF_W_TX_PAYLOAD_NO_ACK 0xB0u
+#define CMD_NRF_NOP                 0x00u
 
 /*------------------------------------------------*/
 /*              NRF24L01 Register Addresses       */
 /*------------------------------------------------*/
-#define REG_NRF_CONFIG              0x00
-#define REG_NRF_EN_AA               0x01
-#define REG_NRF_EN_RXADDR           0x02
-#define REG_NRF_SETUP_AW            0x03
-#define REG_NRF_SETUP_RETR          0x04
-#define REG_NRF_RF_CH               0x05
-#define REG_NRF_RF_SETUP            0x06
-#define REG_NRF_STATUS              0x07
-#define REG_NRF_OBSERVE_TX          0x08
-#define REG_NRF_CD                  0x09
-#define REG_NRF_RX_ADDR_P0          0x0A
-#define REG_NRF_RX_ADDR_P1          0x0B
-#define REG_NRF_RX_ADDR_P2          0x0C
-#define REG_NRF_RX_ADDR_P3          0x0D
-#define REG_NRF_RX_ADDR_P4          0x0E
-#define REG_NRF_RX_ADDR_P5          0x0F
-#define REG_NRF_TX_ADDR             0x10
-#define REG_NRF_RX_PW_P0            0x11
-#define REG_NRF_RX_PW_P1            0x12
-#define REG_NRF_RX_PW_P2            0x13
-#define REG_NRF_RX_PW_P3            0x14
-#define REG_NRF_RX_PW_P4            0x15
-#define REG_NRF_RX_PW_P5            0x16
-#define REG_NRF_FIFO_STATUS         0x17
-#define REG_NRF_DYNPD               0x1C
-#define REG_NRF_FEATURE             0x1D
+#define REG_NRF_CONFIG              0x00u
+#define REG_NRF_EN_AA               0x01u
+#define REG_NRF_EN_RXADDR           0x02u
+#define REG_NRF_SETUP_AW            0x03u
+#define REG_NRF_SETUP_RETR          0x04u
+#define REG_NRF_RF_CH               0x05u
+#define REG_NRF_RF_SETUP            0x06u
+#define REG_NRF_STATUS              0x07u
+#define REG_NRF_OBSERVE_TX          0x08u
+#define REG_NRF_CD                  0x09u
+#define REG_NRF_RX_ADDR_P0          0x0Au
+#define REG_NRF_RX_ADDR_P1          0x0Bu
+#define REG_NRF_RX_ADDR_P2          0x0Cu
+#define REG_NRF_RX_ADDR_P3          0x0Du
+#define REG_NRF_RX_ADDR_P4          0x0Eu
+#define REG_NRF_RX_ADDR_P5          0x0Fu
+#define REG_NRF_TX_ADDR             0x10u
+#define REG_NRF_RX_PW_P0            0x11u
+#define REG_NRF_RX_PW_P1            0x12u
+#define REG_NRF_RX_PW_P2            0x13u
+#define REG_NRF_RX_PW_P3            0x14u
+#define REG_NRF_RX_PW_P4            0x15u
+#define REG_NRF_RX_PW_P5            0x16u
+#define REG_NRF_FIFO_STATUS         0x17u
+#define REG_NRF_DYNPD               0x1Cu
+#define REG_NRF_FEATURE             0x1Du
 
-/*----------------------------------------------*/
-/*                ADDR 0: CONFIG                */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t PRIM_RX:        1;
-        uint8_t PWR_UP:         1;
-        uint8_t CRCO:           1;
-        uint8_t EN_CRC:         1;
-        uint8_t MASK_MAX_RT:    1;
-        uint8_t MASK_TX_DS:     1;
-        uint8_t MASK_RX_DR:     1;
-    };
-} t_NRF_Config;
+/*------------------------------------------------*/
+/*              NRF24L01 Register bits            */
+/*------------------------------------------------*/
+/*        CONFIG        */
+#define NRF_PRX                     0x01u
+#define NRF_PWR_UP                  0x02u
+#define NRF_CRC_2BYTES              0x04u
+#define NRF_IRQ_MAX_RT              0x60u
+#define NRF_IRQ_TX_DS               0x50u
+#define NRF_IRQ_RX_DR               0x30u
+#define NRF_IRQ_NONE                0x70u
 
-/*----------------------------------------------*/
-/*                ADDR 1: ENAA                  */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t ENAA_P0:        1;
-        uint8_t ENAA_P1:        1;
-        uint8_t ENAA_P2:        1;
-        uint8_t ENAA_P3:        1;
-        uint8_t ENAA_P4:        1;
-        uint8_t ENAA_P5:        1;
-    };
-}t_NFR_EN_AA;
+/*    ADDRESS_WIDTH     */
+#define NRF_ADDR_3BYTES             0x01u
+#define NRF_ADDR_4BYTES             0x02u
+#define NRF_ADDR_5BYTES             0x03u
 
-/*----------------------------------------------*/
-/*                ADDR 2: EN_RXADDR             */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t ERX_P0:         1;
-        uint8_t ERX_P1:         1;
-        uint8_t ERX_P2:         1;
-        uint8_t ERX_P3:         1;
-        uint8_t ERX_P4:         1;
-        uint8_t ERX_P5:         1;
-    };
-}t_NRF_EN_RXADDR;
+/*       RF SETUP       */
+#define NRF_POWER_0                 0x00u
+#define NRF_POWER_1                 0x02u
+#define NRF_POWER_2                 0x04u
+#define NRF_POWER_3                 0x06u
+#define NRF_HIGH_DR                 0x08u
 
-/*----------------------------------------------*/
-/*                ADDR 3: SETUP_AW              */
-/*----------------------------------------------*/
-typedef enum{
-    NRF_AW_3_BYTES = 1u,
-    NRF_AW_4_BYTES = 2u,
-    NRF_AW_5_BYTES = 3u
-}e_NRF_Address_Width;
+/*     STATUS MASKS     */
+#define NRF_IS_TX_FULL              0xFEu
+#define NRF_RX_ON_PIPE              0xF1u
+#define NRF_IS_MAX_RT               0xEFu
+#define NRF_IS_TX_SENT              0xDFu
+#define NRF_IS_RX_READY             0xBFu
 
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t AW:             2; /* Address width */
-    };
-}t_NRF_SETUP_AW;
+/*      FIFO MASKS      */
+#define NRF_RX_FIFO_EMPTY           0xFEu
+#define NRF_RX_FIFO_FULL            0xFDu
+#define NRF_TX_FIFO_EMPTY           0xEFu
+#define NRF_TX_FIFO_FULL            0xDFu
+#define NRF_TX_REUSE                0xBFu
 
-/*----------------------------------------------*/
-/*                ADDR 4: SETUP_RETR            */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t ARC:             4; /* Auto Retransmit Count*/
-        uint8_t ARD:             4; /* Auto Retransmit Delay n*250µs*/
-    };
-}t_NRF_SETUP_RETR;
+#define NRF_UNPACK_8(b, p, m) ((b >> p) & m)
 
-/*----------------------------------------------*/
-/*                ADDR 5: RF_CH                 */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t RF_CH:          7; /* RF CH 0 -> 125 */
-    };
-}t_NRF_RF_CH;
+typedef struct
+{
+    uint8_t STATUS;
+    uint8_t CONFIG;
+    uint8_t TX_ADDR[5];
+    uint8_t RF_CHANNEL;
+    uint8_t RF_POWER;
+    uint8_t RF_SETUP;
+    uint8_t ADDR_WIDTH;
+    uint8_t SETUP_RETR;
+    uint8_t ReadFlag;
+} t_NRF_Setup;
 
-/*----------------------------------------------*/
-/*                ADDR 6: RF_SETUP              */
-/*----------------------------------------------*/
-typedef enum{
-    NRF_ePWR_MIN =  0u,
-    NRF_ePWR_LOW =  1u,
-    NRF_ePWR_HIGH = 2u,
-    NRF_ePWR_MAX =  3u
-}e_NRF_RF_Power;
-
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t LNA_HCURR:      1; /* Setup LNA gain */
-        uint8_t RF_PW:          2; /* Set RF power */
-        uint8_t RF_DR:          1; /* Set aire data rate */
-        uint8_t PLL_LOCK:       1; /* Only used in test */
-    };
-}t_NRF_RF_SETUP;
-
-/*----------------------------------------------*/
-/*                ADDR 7: STATUS                */
-/*----------------------------------------------*/
-typedef enum{
-    NRF_ePIPE0 = 0u,
-    NRF_ePIPE1 = 1u,
-    NRF_ePIPE2 = 2u,
-    NRF_ePIPE3 = 3u,
-    NRF_ePIPE4 = 4u,
-    NRF_ePIPE5 = 5u,
-    NRF_eEMPTY = 7u
-            
-}e_NRF_RX_P_No;
-
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t TX_FULL:        1;
-        uint8_t RX_P_NO:        3;
-        uint8_t MAX_RT:         1;
-        uint8_t TX_DS:          1;
-        uint8_t RX_DR:          1;
-    };
-}t_NRF_STATUS;
-
-/*----------------------------------------------*/
-/*                ADDR 8: OBSERVE_TX            */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t ACR_CNT:        4; /* Number of retransmitted packets */
-        uint8_t PLOS_CNT:       4; /* Number of lost packets */
-    };
-}t_NRF_OBSERVE_TX;
-
-/*----------------------------------------------*/
-/*                ADDR 9: CARRIVER_DETECT       */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t CD: 1;
-    };
-}t_NRF_DC;
-
-/*----------------------------------------------*/
-/*                ADDR 10-11: RX_ADDR_P0-P1     */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t ByteArray[5];
-    struct {
-        uint8_t Byte0;
-        uint8_t Byte1;
-        uint8_t Byte2;
-        uint8_t Byte3;
-        uint8_t Byte4;
-    };
-}t_NRF_RX_ADDR_5_BYTES;
-typedef unsigned char t_NRF_RX_ADDR_1_BYTE;
-
-/*----------------------------------------------*/
-/*                ADDR 23: FIFO_STATUS          */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t RX_EMPTY:       1;
-        uint8_t RX_FULL:        1;
-        uint8_t reserved:       2;
-        uint8_t TX_EMPTY:       1;
-        uint8_t TX_FULL:        1;
-        uint8_t TX_REUSE:       1;
-    };
-}t_NRF_FIFO_STATUS;
-
-/*----------------------------------------------*/
-/*                ADDR 28: DYNPD                */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t DPL_P0:          1;
-        uint8_t DPL_P1:          1;
-        uint8_t DPL_P2:          1;
-        uint8_t DPL_P3:          1;
-        uint8_t DPL_P4:          1;
-        uint8_t DPL_P5:          1;
-    };
-}t_NRF_DYNPD;
-
-/*----------------------------------------------*/
-/*                ADDR 29: FEATURE              */
-/*----------------------------------------------*/
-typedef union {
-    uint8_t byte;
-    struct {
-        uint8_t EN_DYN_ACK:     1;
-        uint8_t EN_ACK_PAY:     1;
-        uint8_t EN_DPL:         1;
-    };
-}t_NRF_FEATURE;
-
-extern uint8_t (*p_NRF_SPI_Exchange)(uint8_t);
+typedef struct
+{
+    uint8_t PIPE_SETUP;
+    uint8_t PIPE_ADDR[5];
+    uint8_t PAYLOAD_LEN;
+} t_NRF_RX_PIPE;
 
 void NRF24L01_Init(void);
-void NRF_SET_PRX(void);
+void NRF_Set_PRX(t_NRF_Setup *MyNRF);
+void NRF_Set_PTX(t_NRF_Setup *MyNRF);
+void NRF_SetRFChannel(t_NRF_Setup *MyNRF, uint8_t RF_Channel);
+void NRF_SetRFPower(t_NRF_Setup *MyNRF, uint8_t RF_Pow);
+void NRF_SetRFDataRate(t_NRF_Setup *MyNRF, uint8_t Datarate);
+void NRF_SetAddrWidth(t_NRF_Setup *MyNRF, uint8_t Width);
 uint8_t NRF_Write_Register(uint8_t Register, uint8_t *Bytes, uint8_t Length);
 uint8_t NRF_Read_Register(uint8_t Register, uint8_t *Bytes, uint8_t Length);
 void NRF_IRQ_ISR_Handler(void);
